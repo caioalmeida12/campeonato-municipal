6b632e459ea6503692e1a18b51615dd8638593b0
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-12-15 07:46:52 -0300

    feat: corrigi a mensagem de erro para buscas vazias do sequelize

--- lib/responses/sequelizeEmptyResponse.ts
+++ lib/responses/sequelizeEmptyResponse.ts
@@ -1,7 +1,7 @@
 class SequelizeEmptyResponse extends Error {
   constructor(query: Record<string, unknown>) {
     if (Object.keys(query).length) super(`Empty result for field(s) ${Object.keys(query).join(", ")} with value(s) ${Object.values(query).join(", ")}`);
-    else super("Empty result");
+    else super("Nenhum resultado encontrado");
   }
 }
 
