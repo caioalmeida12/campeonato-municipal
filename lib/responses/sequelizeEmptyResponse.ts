4767cf14cd3dfb613bed3d712130278979279d75
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-12-17 15:58:39 -0300

    feat: traduzi o texto da mensagem de resultado vazio do sequelize

--- lib/responses/sequelizeEmptyResponse.ts
+++ lib/responses/sequelizeEmptyResponse.ts
@@ -1,6 +1,6 @@
 class SequelizeEmptyResponse extends Error {
   constructor(query: Record<string, unknown>) {
-    if (Object.keys(query).length) super(`Empty result for field(s) ${Object.keys(query).join(", ")} with value(s) ${Object.values(query).join(", ")}`);
+    if (Object.keys(query).length) super(`Nenhum resultado para o(s) campo(s) ${Object.keys(query).join(", ")} com valor(es) ${Object.values(query).join(", ")}`);
     else super("Nenhum resultado encontrado");
   }
 }
