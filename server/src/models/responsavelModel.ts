53c9aa5d0bc83ea1a6de05e6f7b5d9e27cff21e6
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-12-17 20:55:56 -0300

    feat: DefaultScope dos responsaveis agora inclui o jogador

--- server/src/models/responsavelModel.ts
+++ server/src/models/responsavelModel.ts
@@ -1,12 +1,10 @@
 import { ResponsavelType } from '@lib/types/responsavelType';
-import { AllowNull, Column, Length, Table, DataType, Model, ForeignKey, BelongsTo, HasMany, Min, Max, PrimaryKey, Unique, Scopes, DefaultScope, Default} from "sequelize-typescript";
+import { AllowNull, Column, Length, Table, DataType, Model, ForeignKey, BelongsTo, HasMany, Min, Max, PrimaryKey, Unique, Scopes, DefaultScope, Default, HasOne } from "sequelize-typescript";
 
 import JogadorModel from './jogadorModel';
 
 @DefaultScope(() => ({
-    attributes: {
-        exclude: ["createdAt", "updatedAt", "deletedAt"],
-    },
+    include: [JogadorModel.unscoped()]
 }))
 @Table({
     tableName: process.env.MODEL_RESPONSAVEL_TABLE_NAME,
@@ -14,23 +12,23 @@ import JogadorModel from './jogadorModel';
 })
 export default class ResponsavelModel extends Model<ResponsavelType, Omit<ResponsavelType, "id">> {
     @PrimaryKey
-    @Length({ min: 36, max: 36 })
     @Default(DataType.UUIDV4)
-    @Column(DataType.UUID)
+    @Column(DataType.UUIDV4)
     declare id: string;
 
+    @Unique
     @AllowNull(false)
     @Length({ min: 11, max: 11 })
     @Column(DataType.STRING(11))
     declare cpf: string;
 
     @AllowNull(false)
-    @Length({ min: 8, max: 256 })
-    @Column(DataType.STRING(256))
+    @Length({ min: 1, max: 128 })
+    @Column(DataType.STRING(128))
     declare nome_completo: string;
 
     @AllowNull(false)
-    @Length({ min: 1, max: 13 })
+    @Length({ min: 11, max: 13 })
     @Column(DataType.STRING(13))
     declare telefone: string;
 
@@ -39,10 +37,8 @@ export default class ResponsavelModel extends Model<ResponsavelType, Omit<Respon
     @Column(DataType.STRING(128))
     declare email: string;
 
-    @AllowNull(false)
-    @Length({ min: 36, max: 36 })
     @ForeignKey(() => JogadorModel)
-    @Column(DataType.UUID)
+    @Column(DataType.UUIDV4)
     declare fk_jogador_id: string;
 
     @BelongsTo(() => JogadorModel, {
--- server/src/repositories/responsavelRepository.ts
+++ server/src/repositories/responsavelRepository.ts
@@ -2,18 +2,28 @@ import ResponsavelModel from "@server/models/responsavelModel";
 import { Op } from "sequelize";
 
 class ResponsavelRepository {
+    async findAll(camposParaBusca?: Array<{ campo: string, valor: unknown }>): Promise<ResponsavelModel[]> {
+        const filtros = {
+            responsavel: camposParaBusca?.map(campoParaBusca => ({ [campoParaBusca.campo]: campoParaBusca.valor })),
+            jogador: camposParaBusca?.map(campoParaBusca => ({ [`$jogador.${campoParaBusca.campo}$`]: campoParaBusca.valor }))
+        }
+
+        const where = {
+            [Op.or]: {
+                [Op.and]: filtros.responsavel,
+                [Op.or]: filtros.jogador
+            }
+        }
+
+        if (!camposParaBusca?.length) return ResponsavelModel.findAll();
+        
+        return ResponsavelModel.findAll({
+            where
+        });
+    }
+
     async create(responsavel: ResponsavelModel) {
         return await ResponsavelModel.create(responsavel);
-    }
-    
-    async findAll(camposParaBusca?: Array<{ campo: string, valor: unknown }>): Promise<ResponsavelModel[]> {
-        const where = { [Op.and]: [{}], [Op.or]: [{}]}
-
-        if (camposParaBusca?.includes({ campo: "nome_completo", valor: "" })) where[Op.or].push({ "$responsavel.nome_completo$": { [Op.like]: `%${camposParaBusca.find(campoParaBusca => campoParaBusca.campo === "nome_completo")?.valor}%` } })
-
-        camposParaBusca?.map(campoParaBusca => where[Op.and].push({ [campoParaBusca.campo]: campoParaBusca.valor }))
-
-        return ResponsavelModel.findAll({ where });
     }
 }
 
