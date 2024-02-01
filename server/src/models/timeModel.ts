300233293f86265a9d64a409900b250e2acc04d0
caioalmeida12 <araujo.caio08@aluno.ifce.edu.br>
2024-02-01 09:49:05 -0300

    temp: enviando arquivos para acesso em outro computador

---
+++ server/src/models/timeModel.ts
@@ -0,0 +1,59 @@
+import { ResponsavelType } from '@lib/types/responsavelType';
+import { AllowNull, Column, Length, DataType, Model, ForeignKey, BelongsTo, PrimaryKey, Unique, DefaultScope, Default, Table } from "sequelize-typescript";
+
+import EsporteModel from './esporteModel';
+
+@DefaultScope(() => ({
+    include: [EsporteModel.unscoped()]
+}))
+@Table({
+    tableName: process.env.MODEL_TIME_TABLE_NAME,
+    paranoid: true,
+})
+export default class TimeModel extends Model<ResponsavelType, Omit<ResponsavelType, "id">> {
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column(DataType.UUIDV4)
+    declare id: string;
+
+    @Unique
+    @AllowNull(false)
+    @Length({ min: 1, max: 64 })
+    @Column(DataType.STRING(64))
+    declare nome: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 128 })
+    @Column(DataType.STRING(128))
+    declare localidade: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 128 })
+    @Column(DataType.STRING(128))
+    declare responsavel: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 13 })
+    @Column(DataType.STRING(13))
+    declare telefone: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 128 })
+    @Column(DataType.STRING(128))
+    declare email: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 256 })
+    @Column(DataType.STRING(256))
+    declare escudo: string;
+
+    @ForeignKey(() => EsporteModel)
+    @Column(DataType.UUIDV4)
+    declare fk_esporte_id: string;
+
+    @BelongsTo(() => EsporteModel, {
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+    })
+    declare esporte: EsporteModel;
+}
---
+++ server/src/repositories/timeRepository.ts
@@ -0,0 +1,49 @@
+import { TimeType } from "@lib/types/timeType";
+import sequelize from "@server/database/connection";
+import TimeModel from "@server/models/timeModel";
+import JogadorModel from "@server/models/jogadorModel";
+import { Op } from "sequelize";
+
+class TimeRepository {
+    async findAll(camposParaBusca?: Array<{ campo: string, valor: unknown }>): Promise<TimeModel[]> {
+        const filtros = {
+            time: camposParaBusca?.map(campoParaBusca => ({ [campoParaBusca.campo]: campoParaBusca.valor })),
+        }
+
+        const where = {
+            [Op.and]: filtros.time,
+        }
+
+        if (!camposParaBusca?.length) return TimeModel.findAll();
+
+        return TimeModel.findAll({
+            where
+        });
+    }
+
+    async create(body: Omit<TimeType, "id">): Promise<TimeModel> {
+        const resultado = await sequelize.transaction(async (t) => {
+            const time = await TimeModel.create(body, {
+                transaction: t,
+                include: [JogadorModel.unscoped()]
+            });
+
+            return time;
+        });
+
+        return resultado;
+    }
+
+    async update(id: string, body: Omit<TimeType, "fk_jogador_id">): Promise<TimeModel | undefined> {
+        const time = await TimeModel.findByPk(id);
+
+        return time?.update(body);
+    }
+
+    async delete(id: string): Promise<void | undefined> {
+        const time = await TimeModel.findByPk(id);
+
+        return time?.destroy();
+    }
+}
+
