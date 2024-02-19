4787c85b43fef53398db5b4a758c190d35a6f678
caioalmeida12 <araujo.caio08@aluno.ifce.edu.br>
2024-02-19 10:57:21 -0300

    temp: criei o type e o model do time

---
+++ lib/types/timeType.ts
@@ -0,0 +1,40 @@
+import z from 'zod';
+
+const timeSchema = z.object({
+    id: z.preprocess(
+        (val) => String(val),
+        z.string().uuid()
+    ),
+    nome: z.preprocess(
+        (val) => String(val),
+        z.string().min(1).max(64)
+    ),
+    localidade: z.preprocess(
+        (val) => String(val),
+        z.string().min(1).max(128)
+    ),
+    resposavel: z.preprocess(
+        (val) => String(val),
+        z.string().min(1).max(128)
+    ),
+    telefone: z.preprocess(
+        (val) => String(val),
+        z.string().min(1).max(13)
+    ),
+    email: z.preprocess(
+        (val) => String(val),
+        z.string().email()
+    ),
+    escudo: z.preprocess(
+        (val) => String(val),
+        z.string().min(1).max(256)
+    ),
+    fk_esporte_id: z.preprocess(
+        (val) => String(val),
+        z.string().uuid()
+    ),
+    esporte: z.any().optional(),
+});
+
+export default timeSchema;
+export type TimeType = z.infer<typeof timeSchema>;
---
+++ server/src/models/timeModel.ts
@@ -0,0 +1,61 @@
+import { TimeType } from '@lib/types/timeType';
+import { AllowNull, Column, Length, Table, DataType, Model, ForeignKey, BelongsTo, PrimaryKey, Unique, Default } from "sequelize-typescript";
+
+import Esporte from './esporteModel';
+
+@Table({
+    tableName: process.env.MODEL_POSICAO_TABLE_NAME,
+    paranoid: true,
+})
+export default class TimeModel extends Model<TimeType> {
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column(DataType.UUIDV4)
+    declare id: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 64 })
+    @Unique
+    @Column(DataType.STRING(64))
+    declare nome: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 128 })
+    @Unique
+    @Column(DataType.STRING(128))
+    declare localidade: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 128 })
+    @Unique
+    @Column(DataType.STRING(128))
+    declare responsavel: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 13 })
+    @Unique
+    @Column(DataType.STRING(13))
+    declare telefone: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 128 })
+    @Unique
+    @Column(DataType.STRING(128))
+    declare email: string;
+
+    @AllowNull(false)
+    @Length({ min: 1, max: 256 })
+    @Unique
+    @Column(DataType.STRING(256))
+    declare escudo: string;
+
+    @ForeignKey(() => Esporte)
+    @Column(DataType.UUIDV4)
+    declare fk_esporte_id: string;
+
+    @BelongsTo(() => Esporte, {
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+    })
+    declare esporte: Esporte;
+}
