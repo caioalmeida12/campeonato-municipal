02520e96bc0e38c02b88fa8e301aa9be7eb1785d
LuanF11 <luaninfo09@gmail.com>
2024-03-15 00:46:36 -0300

    Criado a pasta dos icones e adicionado o icone Plus

---
+++ client/src/components/Icons/PlusIcon.tsx
@@ -0,0 +1,9 @@
+const PlusIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="14" viewBox="0 0 15 14" fill="none">
+            <path d="M14.1621 8H8.16211V14H6.16211V8H0.162109V6H6.16211V0H8.16211V6H14.1621V8Z" fill="white"/>
+        </svg>
+    )
+}
+
+export default PlusIcon;
