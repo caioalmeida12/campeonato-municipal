8f7d7dbf8eaf03ed12807784fa79121d0b8eeb1d
LuanF11 <luaninfo09@gmail.com>
2024-03-15 00:50:25 -0300

    Criado icone de filtro

---
+++ client/src/components/Icons/FilterIcon.tsx
@@ -0,0 +1,9 @@
+const FilterIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="13" viewBox="0 0 20 13" fill="none">
+            <path d="M8.04393 12.5H12.0869V10.5H8.04393V12.5ZM0.96875 0.5V2.5H19.1621V0.5H0.96875ZM4.00097 7.5H16.1298V5.5H4.00097V7.5Z" fill="black" fill-opacity="0.5"/>
+        </svg>
+    )
+}
+
+export default FilterIcon;
