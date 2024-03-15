5e06c97cba997f2967c240413e7eb32f3fcf92a0
LuanF11 <luaninfo09@gmail.com>
2024-03-15 00:42:16 -0300

    Criado uma variação do label

--- client/src/components/Label/Label.tsx
+++ client/src/components/Label/Label.tsx
@@ -3,15 +3,16 @@ import styles from "./Label.module.css";
 
 interface LabelProps {
   children: React.ReactNode;
-  variant: "simples";
+  variant: "labelBig" | "labelSmall";
 }
 
 const variantStyles = {
-  "simples": styles.simple,
+    "labelBig": styles.labelBig,
+    "labelSmall": styles.labelSmall,
 };
 
 const Label = ({ children, variant }: LabelProps) => {
-  const style = `${styles.label} ${variantStyles[variant]}`;
+  const style = `${styles.labelBig} ${variantStyles[variant]}`;
 
   return (
     <label className={style}>{children}</label>
