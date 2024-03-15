de10d3fbc1fd182da95ab61eec7f9657b20187c8
caioalmeida12 <araujo.caio08@aluno.ifce.edu.br>
2024-03-15 13:16:22 -0300

    fix: corrigi as funções do tableHelper

--- client/src/app/(pages)/times/page.tsx
+++ client/src/app/(pages)/times/page.tsx
@@ -11,60 +11,59 @@ import { handleCreate, handleDelete, handleGet } from "@/lib/tableHelper"
 const Times = () => {
     const [data, setData] = useState<TimeType[]>([])
     const [shouldRefetch, setShouldRefetch] = useState(false)
-    const [error, setError] = useState<Error | null>()
+    const [error, setError] = useState<string | null>()
 
     useEffect(() => {
-        handleGet<TimeType>("times", setData)
+        const fetchData = async () => {
+            handleGet<TimeType>("times")
+                .then((response) => {
+                    response.success ? setData(response.response as []) : setError(response.response as any)
+                })
+        }
+
+        fetchData()
     }, [shouldRefetch]);
 
-    useEffect(() => {
-        console.log(error)
-    })
-
     const handleCreateButton = async () => {
-        try {
-            const esportePost: EsporteType = {
-                id: "",
-                nome: "Esporte 1",
-                maximo_jogadores_por_time: 10,
-                maximo_jogadores_titulares: 10
-            }
-
-            const timePost: TimeType = {
-                id: "",
-                nome: "Time 1",
-                email: "acasdasda@gmail.com",
-                telefone: "1231231231232",
-                escudo: "https://www.google.com.br",
-                fk_esporte_id: "",
-                localidade: "São Paulo",
-                responsavel: "Responsável 1",
-            }
-
-            const esporte = await fetch(`http://localhost:5000/esportes`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-JWT-token": localStorage.getItem("cm-jwt-token") || ""
-                },
-                body: JSON.stringify(esportePost)
-            }).then(data => data.json())
-
-            timePost.fk_esporte_id = esporte.id
-
-            const time = handleCreate<TimeType>("times", timePost, setData)
-
-            console.log(time)
-        } catch (error) {
-            setError(error as Error)
+        const esportePost: EsporteType = {
+            id: "",
+            nome: "Esporte 1",
+            maximo_jogadores_por_time: 10,
+            maximo_jogadores_titulares: 10
         }
+
+        const timePost: TimeType = {
+            id: "",
+            nome: "Time 1",
+            email: "acasdasda@gmail.com",
+            telefone: "1231231231232",
+            escudo: "https://www.google.com.br",
+            fk_esporte_id: "",
+            localidade: "São Paulo",
+            responsavel: "Responsável 1",
+        }
+
+        const esporte = await fetch(`http://localhost:5000/esportes`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-JWT-token": localStorage.getItem("cm-jwt-token") || ""
+            },
+            body: JSON.stringify(esportePost)
+        }).then(data => data.json())
+
+        timePost.fk_esporte_id = esporte.id
+
+        const create = await handleCreate<TimeType>("times", timePost)
+
+        create.success ? setData(create.response as []) : setError(create.response as any);
     }
 
     const handleDeleteButton = (id: string) => {
         try {
             handleDelete("times", id, shouldRefetch, setShouldRefetch)
-        } catch (error) {
-            setError(error as Error)
+        } catch (error: any) {
+            setError(error)
         }
     }
 
@@ -80,7 +79,7 @@ const Times = () => {
                     />
                 )
             }
-            {error && error.message}
+            {error}
         </Structure>
     )
 }
